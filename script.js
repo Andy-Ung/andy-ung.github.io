3c7573596b7563a24312b6871bb173ca4a8727e7
Andy-Ung <75831215+Andy-Ung@users.noreply.github.com>
2025-04-07 18:07:03 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
-
 function openCity(evt, cityName) {
   // Declare all variables
   var i, tabcontent, tablinks;
